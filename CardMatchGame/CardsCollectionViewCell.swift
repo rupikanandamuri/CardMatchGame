9d03bfa18c9445f3cc50c7c4a2fec8eb58f4e1e3
rupikanandamuri <rupikachowdhary09@gmail.com>
2019-09-10 20:17:12 -0400

    first code

---
+++ CardMatchGame/CardsCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CardsCollectionViewCell.swift
+//  CardMatchGame
+//
+//  Created by Rupika on 2019-09-10.
+//  Copyright © 2019 Rupika. All rights reserved.
+//
+
+import UIKit
+
+class CardsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var  cardView : UIView!
+    
+}
--- CardMatchGame/ViewController.swift
+++ CardMatchGame/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    @IBOutlet var collectionView : UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardcell", for: indexPath) as! CardsCollectionViewCell
+        cell.cardView.layer.backgroundColor = getRandomColor().cgColor
+        return cell
+    }
+    //MARK - Randome color generator.
+    
+    func random() -> CGFloat{
+        
+        return CGFloat(arc4random()) /  CGFloat(UInt32.max)
+    }
+    func getRandomColor() -> UIColor {
+        return UIColor(red: random(), green: random(), blue: random(), alpha: 1)
+    }
+    
 }
 
